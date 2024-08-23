51a3c3b1f9e5e000f3bb77352050405b669a07e5
Taka Oyama <toyama4649@hotmail.com>
2024-08-23 22:57:58 +0900

    add simple test

--- tests/src/FileMutexTest.php
+++ tests/src/FileMutexTest.php
@@ -5,14 +5,50 @@ namespace Tests\Kirameki\Mutex;
 use Kirameki\Core\Testing\TestCase;
 use Kirameki\Mutex\Configs\FileMutexConfig;
 use Kirameki\Mutex\FileMutex;
+use PHPUnit\Framework\Attributes\After;
+use PHPUnit\Framework\Attributes\Before;
 use function dump;
+use function glob;
+use function scandir;
+use function unlink;
 
 final class FileMutexTest extends TestCase
 {
-    public function test_locking(): void
+    protected string $fileDir = '/tmp/kirameki-mutex-test';
+
+    #[Before]
+    protected function createTempDir(): void
     {
-        $mutex = new FileMutex(new FileMutexConfig('/tmp'));
+        if (!file_exists($this->fileDir)) {
+            mkdir($this->fileDir);
+        }
+    }
+
+    /**
+     * @return list<string>
+     */
+    protected function scanTempDir(string $pattern = '*'): array
+    {
+        return glob("{$this->fileDir}/{$pattern}") ?: [];
+    }
+
+    #[After]
+    protected function removeTempDir(): void
+    {
+        if (file_exists($this->fileDir)) {
+            array_map(unlink(...), $this->scanTempDir());
+            rmdir($this->fileDir);
+        }
+    }
+
+    public function test_tryAcquire(): void
+    {
+        $mutex = new FileMutex(new FileMutexConfig($this->fileDir));
         $lock = $mutex->tryAcquire('test');
-        $lock?->release();
+        $this->assertNotNull($lock);
+
+        $this->assertCount(1, $this->scanTempDir("mutex_*"));
+        $lock->release();
+        $this->assertCount(0, $this->scanTempDir("mutex_*"));
     }
 }
